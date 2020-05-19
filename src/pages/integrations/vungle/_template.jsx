3c431c3457129e675ddca2d556fb63c03423a75e
Jake Austin <jaustin@mparticle.com>
2020-05-19 10:52:35 -0400

    override vungle logo (#913)

--- src/pages/integrations/vungle/_template.jsx
+++ src/pages/integrations/vungle/_template.jsx
@@ -1,5 +1,6 @@
 export const frontmatter = {
     partnerId: 1159,
     title: 'Vungle',
-    redirect: '/integrations/vungle/audience/'
+    redirect: '/integrations/vungle/audience/',
+    partnerImageOverride: 'https://static.mparticle.com/providerimg/forwarderlogo_1159_Vungle.svg'
 };
