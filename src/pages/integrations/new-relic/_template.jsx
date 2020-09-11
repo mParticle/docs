f6452312d986f183b31bb5fbb0b82418e8ed6f15
Jake Austin <jaustin@mparticle.com>
2020-09-11 14:50:01 -0400

    remove new relic docs (#1114)
    Co-authored-by: Jake Austin <jakeaust07@gmail.com>

--- src/pages/integrations/new-relic/_template.jsx
+++
@@ -1,6 +0,0 @@
-export const frontmatter = {
-    partnerId: 1158,
-    title: 'New Relic',
-    redirect: '/integrations/new-relic/event/',
-    partnerImageOverride: 'https://static.mparticle.com/providerimg/forwarderlogo_1158_NewRelic,Inc.svg'
-};
