302984c35662398c6efbec603d1d6afc6ff7911e
Jake Austin <jaustin@mparticle.com>
2020-11-09 13:42:55 -0500

    add override for tiktok logo (#1181)
    Co-authored-by: Jake Austin <jakeaust07@gmail.com>

--- src/pages/integrations/tiktok/_template.jsx
+++ src/pages/integrations/tiktok/_template.jsx
@@ -1,5 +1,6 @@
 export const frontmatter = {
     partnerId: 1170,
     title: 'TikTok',
-    redirect: '/integrations/tiktok/audience/'
+    redirect: '/integrations/tiktok/audience/',
+    partnerImageOverride: 'https://static.mparticle.com/providerimg/forwarderlogo_1170_TikTok.svg'
 };
