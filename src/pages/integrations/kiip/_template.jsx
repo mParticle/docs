2c939f66591cedd5a109a09c30af1b5e2d8ffb36
Jake Austin <jaustin@mparticle.com>
2020-11-12 12:48:39 -0500

    remove kiip docs (#1195)
    Co-authored-by: Jake Austin <jakeaust07@gmail.com>

--- src/pages/integrations/kiip/_template.jsx
+++
@@ -1,6 +0,0 @@
-export const frontmatter = {
-    partnerId: 1086,
-    title: 'Kiip',
-    redirect: '/integrations/kiip/audience/',
-    partnerImageOverride: 'https://static.mparticle.com/providerimg/forwarderlogo_1086_Kiip.svg'
-};
