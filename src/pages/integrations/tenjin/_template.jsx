89f01708deb8d540c9e4ef190718836dcae0dba1
Jake Austin <jaustin@mparticle.com>
2020-11-12 12:48:50 -0500

    remove tenjin docs (#1194)
    Co-authored-by: Jake Austin <jakeaust07@gmail.com>

--- src/pages/integrations/tenjin/_template.jsx
+++
@@ -1,6 +0,0 @@
-export const frontmatter = {
-    partnerId: 1084,
-    title: 'Tenjin',
-    redirect: '/integrations/tenjin/event/',
-    partnerImage: 'https://static.mparticle.com/providerimg/forwarderlogo_1084_Tenjin.svg'
-};
