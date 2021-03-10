3ded5b25e306b9b6d37f8f85d159d76f4b0c7d83
Jake Austin <jaustin@mparticle.com>
2021-03-10 09:13:48 -0500

    remove fullstory from docsite (#1332)
    Co-authored-by: Jake Austin <jakeaust07@gmail.com>

--- src/pages/integrations/fullstory/_template.jsx
+++
@@ -1,5 +0,0 @@
-export const frontmatter = {
-    title: 'FullStory',
-    redirect: '/integrations/fullstory/event/',
-    partnerImageOverride: 'https://static.mparticle.com/providerimg/forwarderlogo_9003_FullStory.svg'
-};
