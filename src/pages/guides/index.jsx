8a5e5a095eeb667b9522d0248f4ceb962ef3e2b9
Mysti Berry <mberry@mparticle.com>
2022-01-05 14:06:09 -0800

    Add changelog link to guides landing page (#1665)
    * addressing feedback
    
    * Fix lint issues
    
    Co-authored-by: Alexander Sapountzis <alexs@mparticle.com>

--- src/pages/guides/index.jsx
+++ src/pages/guides/index.jsx
@@ -14,102 +14,116 @@ const Links = [
         route: 'platform-guide',
         title: 'Platform Guide',
         content: 'Learn to manage your data in the mParticle dashboard.',
-        links: [{
-            text: 'Activity',
-            link: 'activity',
-        },
-        {
-            text: 'Audiences',
-            link: 'audiences',
-        },
-        {
-            text: 'Calculated Attributes',
-            link: 'calculated-attributes',
-        },
-        {
-            text: 'Connections',
-            link: 'connections',
-        },
-        {
-            text: 'Data Filter',
-            link: 'data-filter',
-        }],
+        links: [
+            {
+                text: 'Activity',
+                link: 'activity',
+            },
+            {
+                text: 'Audiences',
+                link: 'audiences',
+            },
+            {
+                text: 'Calculated Attributes',
+                link: 'calculated-attributes',
+            },
+            {
+                text: 'Connections',
+                link: 'connections',
+            },
+            {
+                text: 'Data Filter',
+                link: 'data-filter',
+            },
+        ],
     },
     {
         route: 'getting-started',
         title: 'Getting Started',
-        content: 'A from-scratch guide to get you started sending data to mParticle and forwarding it on to Event and Audience outputs.',
-        links: [{
-            text: 'Create Inputs',
-            link: 'create-an-input',
-        },
-        {
-            text: 'Start capturing data',
-            link: 'start-capturing-data',
-        },
-        {
-            text: 'Connect an Event Output',
-            link: 'connect-an-event-output',
-        },
-        {
-            text: 'Create an Audience',
-            link: 'create-an-audience',
-        }],
+        content:
+            'A from-scratch guide to get you started sending data to mParticle and forwarding it on to Event and Audience outputs.',
+        links: [
+            {
+                text: 'Create Inputs',
+                link: 'create-an-input',
+            },
+            {
+                text: 'Start capturing data',
+                link: 'start-capturing-data',
+            },
+            {
+                text: 'Connect an Event Output',
+                link: 'connect-an-event-output',
+            },
+            {
+                text: 'Create an Audience',
+                link: 'create-an-audience',
+            },
+        ],
     },
     {
         route: 'idsync',
         title: 'IDSync',
-        content: 'Learn about mParticle’s premium Identity Management Framework',
-        links: [{
-            text: 'Introduction',
-            link: 'introduction',
-        },
-        {
-            text: 'Use Cases for IDSync',
-            link: 'use-cases',
-        },
-        {
-            text: 'Components of IDSync',
-            link: 'components',
-        }],
+        content:
+            'Learn about mParticle’s premium Identity Management Framework',
+        links: [
+            {
+                text: 'Introduction',
+                link: 'introduction',
+            },
+            {
+                text: 'Use Cases for IDSync',
+                link: 'use-cases',
+            },
+            {
+                text: 'Components of IDSync',
+                link: 'components',
+            },
+        ],
     },
     {
         route: 'data-master',
         title: 'Data Master',
-        content: 'Explore every data point in your workspace and manage your data quality.',
-        links: [{
-            text: 'Data Master Introduction',
-            link: 'introduction',
-        },
-        {
-            text: 'Catalog',
-            link: 'catalog',
-        },
-        {
-            text: 'Live Stream',
-            link: 'live-stream',
-        },
-        {
-            text: 'Data Plan',
-            link: 'data-planning',
-        }],
+        content:
+            'Explore every data point in your workspace and manage your data quality.',
+        links: [
+            {
+                text: 'Data Master Introduction',
+                link: 'introduction',
+            },
+            {
+                text: 'Catalog',
+                link: 'catalog',
+            },
+            {
+                text: 'Live Stream',
+                link: 'live-stream',
+            },
+            {
+                text: 'Data Plan',
+                link: 'data-planning',
+            },
+        ],
     },
     {
         route: 'data-privacy-controls',
         title: 'Data Privacy Controls',
-        content: 'Collect and leverage consumer consent and opt-outs towards compliance with GDPR and CCPA.',
+        content:
+            'Collect and leverage consumer consent and opt-outs towards compliance with GDPR and CCPA.',
         links: [{}],
     },
     {
         route: 'data-subject-requests',
         title: 'Data Subject Requests',
-        content: 'Explore how to respond to data subject requests as mandated by the GDPR and CCPA regulations.',
+        content:
+            'Explore how to respond to data subject requests as mandated by the GDPR and CCPA regulations.',
         links: [{}],
     },
     {
         route: 'default-service-limits',
         title: 'Default Service Limits',
-        content: 'Learn about the default limits mParticle imposes on incoming data in order to protect the performance of the mParticle dashboard and your app.',
+        content:
+            'Learn about the default limits mParticle imposes on incoming data in order to protect the performance of the mParticle dashboard and your app.',
         links: [{}],
     },
     {
@@ -121,7 +135,8 @@ const Links = [
     {
         route: 'cross-account-audience-sharing/',
         title: 'Cross-Account Audience Sharing',
-        content: 'Share audience data with other accounts within your organization.',
+        content:
+            'Share audience data with other accounts within your organization.',
         links: [{}],
     },
 ];
@@ -133,19 +148,30 @@ const Guides = (props) => (
         <div className='guides main-content'>
             <h1 id='developer-hub-title'>Guides</h1>
             <p className='large'>
-                This section contains in-depth information about mParticle’s platform and features. For implementation information, try the
-                {' '}
-                <Link to='/developers/'>
-                    Developers
-                </Link>
-                {' '}
-                section.
+                This section contains in-depth information about the mParticle
+                platform and features.
+                <ul>
+                    <li>
+                        For information about our SDKs, APIs, and tools, visit
+                        the
+                        <Link to='/developers/'>Developers</Link>
+                        section.
+                    </li>
+                    <li>
+                        For information about our latest product releases, visit
+                        the
+                        {' '}
+                        <Link to='https://changelog.mparticle.com'>
+                            mParticle Changelog
+                        </Link>
+                        .
+                        {' '}
+                    </li>
+                </ul>
             </p>
             <div className='flex-content guide-tiles'>
                 {Links.map((link) => (
-                    <GuideTile
-                        key={`${link.title}${link.route}`}
-                        data={link} />
+                    <GuideTile key={`${link.title}${link.route}`} data={link} />
                 ))}
             </div>
         </div>
@@ -159,7 +185,7 @@ export const frontmatter = {
 
 export const query = graphql`
     query {
-        pageMetadata (path: {eq: "/guides/"}) {
+        pageMetadata(path: { eq: "/guides/" }) {
             ...BreadcrumbMetadata
         }
     }
