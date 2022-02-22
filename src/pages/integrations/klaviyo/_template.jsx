0b688321f59d3e7a44bd0949074dcaa65d812911
Elias Nilsson <enilsson@mparticle.com>
2022-02-22 14:10:11 -0800

    Update klaviyo template to override partner image (#1730)

--- src/pages/integrations/klaviyo/_template.jsx
+++ src/pages/integrations/klaviyo/_template.jsx
@@ -1,5 +1,6 @@
 export const frontmatter = {
     partnerId: 156,
     title: 'Klaviyo',
-    redirect: '/integrations/klaviyo/audience/'
+    redirect: '/integrations/klaviyo/audience/',
+    partnerImageOverride: 'https://static.mparticle.com/providerimg/forwarderlogo_156_Klaviyo.svg'
 };
