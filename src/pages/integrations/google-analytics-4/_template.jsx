ded4372fc11ab26424d80629c7898ef98404e1d8
Debbie Miller <dmiller@mparticle.com>
2022-03-18 14:02:56 -0400

    Add image override for GA4 (#1757)
    For GA4 - there is no logo shown on the doc site since the module is not availablebydefault yet.    In the _template.jsx file, there needs to be a partnerImageOverride line to specify the location of the logo https://static.mparticle.com/providerimg/forwarderlogo_160_GoogleAnalytics4.svg

--- src/pages/integrations/google-analytics-4/_template.jsx
+++ src/pages/integrations/google-analytics-4/_template.jsx
@@ -2,4 +2,5 @@ export const frontmatter = {
     partnerId: 160,
     title: 'Google Analytics 4',
     redirect: '/integrations/google-analytics-4/event/',
+    partnerImageOverride: 'https://static.mparticle.com/providerimg/forwarderlogo_160_GoogleAnalytics4.svg'
 };
