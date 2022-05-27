494dbab24aa7e251cb57fc7e92b97db6387ff605
Debbie Miller <dmiller@mparticle.com>
2022-05-27 13:14:21 -0400

    Fix Branch s2s logo (#1847)

--- src/pages/integrations/branch-metrics-server/_template.jsx
+++ src/pages/integrations/branch-metrics-server/_template.jsx
@@ -2,5 +2,5 @@ export const frontmatter = {
     partnerId: 1111,
     title: 'Branch S2S Event',
     redirect: '/integrations/branch-metrics-server/event/',
-    partnerImageOverride: '/images/branch-s2s-override.png'
+    partnerImageOverride: '/images/forwarderlogo_80_BranchMetricsS2S.svg'
 };
