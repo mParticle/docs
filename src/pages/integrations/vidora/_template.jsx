58e7feb7626f9afcbd3592af96dfb21e96d64166
Riley Kimball <rkimball@mparticle.com>
2022-07-26 16:59:00 -0700

    coercing logo (#1932)

--- src/pages/integrations/vidora/_template.jsx
+++ src/pages/integrations/vidora/_template.jsx
@@ -1,5 +1,6 @@
 export const frontmatter = {
     partnerId: 1230,
     title: 'Vidora',
-    redirect: '/integrations/vidora/event'
+    redirect: '/integrations/vidora/event',
+    partnerImageOverride: 'https://static.mparticle.com/providerimg/forwarderlogo_1230_Vidora.svg'
 };
